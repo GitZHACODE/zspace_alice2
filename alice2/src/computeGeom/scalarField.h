a220d00df4c50329596df9ccaa1cb09edc8f7932
Taizhong Chen <Taizhong_chen@hotmail.com>
2025-10-19 01:37:13 +0100

    added flag for field apply shapes

--- alice2/src/computeGeom/scalarField.h
+++ alice2/src/computeGeom/scalarField.h
@@ -126,7 +126,8 @@ private:
     std::vector<float> m_field_values;
     std::vector<float> m_normalized_values;
     std::vector<Vec3> m_gradient_field;
-     
+    bool m_has_valid_sdf = false;
+    
     // Helper methods
     inline int get_index(int x, int y) const {
         return y * m_res_x + x;
